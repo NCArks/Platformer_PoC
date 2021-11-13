50766a1a21fa87f6e253a816a860a18dba55cfe5
Enjin <hakurei64@gmail.com>
2021-11-13 12:43:10 +0100

    Map loader manager class

---
+++ source/Engine/map_loader.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "map.h"
+
+class MapLoader
+{
+public:
+	MapLoader();
+	~MapLoader();
+
+private:
+
+};
+
+MapLoader::MapLoader()
+{
+
+}
+
+MapLoader::~MapLoader()
+{
+
+}
