afa9c4410819dec6b60efc65956cd9121a44bea7
Hassim Mazahere Rakotomalala <hassimmazahere@gmail.com>
2022-05-09 00:16:35 +0200

    refactor (engine): method args by ref

--- source/main.cpp
+++ source/main.cpp
@@ -23,12 +23,9 @@ const unsigned int SCR_HEIGHT = 600;
 #define ONE_LOGIC_FRAME std::chrono::microseconds(16666)
 
 // Game logic thread's main function
-void logic(LogicElements* elements, Inputs* inputs, std::chrono::steady_clock* clock) {
-    if (elements == nullptr || inputs == nullptr || clock == nullptr) {
-        return;
-    }
+void logic(LogicElements& elements, Inputs& inputs, std::chrono::steady_clock& clock) {
 
-    Map* p_m = elements->getMap();
+    Map* p_m = elements.getMap();
     Map m;
     if (p_m == nullptr) {
         return;
@@ -37,37 +34,37 @@ void logic(LogicElements* elements, Inputs* inputs, std::chrono::steady_clock* c
         m = *p_m;
     }
 
-    Player* p_player = elements->getP1();
+    Player* p_player = elements.getP1();
     if (p_player == nullptr) {
         return;
     }
 
     NpcGoomba* g = nullptr;
     
-    auto previous_time = clock->now();
-    while (!elements->getShouldClose()) {
-        auto current_time = clock->now();
+    auto previous_time = clock.now();
+    while (!elements.getShouldClose()) {
+        auto current_time = clock.now();
         auto delta_time = current_time - previous_time;
         //std::cout << "elapsed time: " << std::chrono::duration_cast<std::chrono::microseconds>(delta_time).count() << std::endl;
         int frames_passed = (int)(delta_time / ONE_LOGIC_FRAME);
         //std::cout << "number of frames: " << frames_passed << std::endl;
         if (frames_passed > 0) {
             previous_time += frames_passed * ONE_LOGIC_FRAME;
-            for (int i = (int)inputs->getInputQueueSize(); i > 0; --i) {
-                inputs->popQueue();
-                inputs->clearStr();
-                for (const auto key : inputs->getPressedKeys()) {
-                    inputs->addStr(key);
+            for (int i = (int)inputs.getInputQueueSize(); i > 0; --i) {
+                inputs.popQueue();
+                inputs.clearStr();
+                for (const auto key : inputs.getPressedKeys()) {
+                    inputs.addStr(key);
                 }
             }
-            inputs->getPressedKeys();
+            inputs.getPressedKeys();
             //elements.getMap();
 
-            p_player->PlayerUpdate(inputs->getPressedKeys(), frames_passed, m);
+            p_player->PlayerUpdate(inputs.getPressedKeys(), frames_passed, m);
 
             
-            for (int i = 0; i < elements->getEnnemiACount(); i++) {
-                g = elements->getEnnemiA(i);
+            for (int i = 0; i < elements.getEnnemiACount(); i++) {
+                g = elements.getEnnemiA(i);
                 g->ennemiUpdate(frames_passed,m);
             }
         }
@@ -97,11 +94,11 @@ void Cleanup(GLFWwindow* window) {
     glfwTerminate();
 }
 
-int DrawFrame(GLFWwindow* window, ImVec4& clear_color, PlayerSkills& pskills, DisplayVariables& dvar, DisplayElements& display, Inputs* inputs, LogicElements* elements, Map* m, Player* p, Shader& playerIcon, Shader& mapTile) {
+int DrawFrame(GLFWwindow& window, ImVec4& clear_color, PlayerSkills& pskills, DisplayVariables& dvar, DisplayElements& display, Inputs& inputs, LogicElements& elements, Map& m, Player& p, Shader& playerIcon, Shader& mapTile) {
     
     NpcGoomba* goomba = nullptr;
 
-    int ret = glfwWindowShouldClose(window);
+    int ret = glfwWindowShouldClose(&window);
     if (ret) {
         return ret;
     }
@@ -130,22 +127,22 @@ int DrawFrame(GLFWwindow* window, ImVec4& clear_color, PlayerSkills& pskills, Di
 
         ImGui::ColorEdit3("background color", (float*)&clear_color);
 
-        ImGui::Text(("Keys: " + (inputs->getPressedStr() == "" ? "None" : inputs->getPressedStr())).c_str());
+        ImGui::Text(("Keys: " + (inputs.getPressedStr() == "" ? "None" : inputs.getPressedStr())).c_str());
 
-        ImGui::Text(("state: " + p->getState()).c_str());
+        ImGui::Text(("state: " + p.getState()).c_str());
 
-        ImGui::Text(("pos_x: " + std::to_string(p->getPosX())).c_str());
-        ImGui::Text(("pos_y: " + std::to_string(p->getPosY())).c_str());
-        ImGui::Text(("spd_x: " + std::to_string(p->getSpdX())).c_str());
-        ImGui::Text(("spd_y: " + std::to_string(p->getSpdY())).c_str());
+        ImGui::Text(("pos_x: " + std::to_string(p.getPosX())).c_str());
+        ImGui::Text(("pos_y: " + std::to_string(p.getPosY())).c_str());
+        ImGui::Text(("spd_x: " + std::to_string(p.getSpdX())).c_str());
+        ImGui::Text(("spd_y: " + std::to_string(p.getSpdY())).c_str());
 
         ImGui::Text("%.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
 
-        ImGui::SliderFloat(std::string("walk_spd").c_str(), &(p->getRefWalkSpd()), .0f, 32.0f);
-        ImGui::SliderFloat(std::string("jump_spd").c_str(), &(p->getRefJumpSpd()), .0f, 32.0f);
-        ImGui::SliderFloat(std::string("gravity").c_str(), &(p->getRefGravityConst()), .0f, 32.0f);
-        ImGui::SliderFloat(std::string("max_fall_spd").c_str(), &(p->getRefMaxFallSpd()), -32.0f, .0f);
-        ImGui::SliderFloat(std::string("min_jump_spd").c_str(), &(p->getRefMinJumpSpd()), .0f, 32.0f);
+        ImGui::SliderFloat(std::string("walk_spd").c_str(), &(p.getRefWalkSpd()), .0f, 32.0f);
+        ImGui::SliderFloat(std::string("jump_spd").c_str(), &(p.getRefJumpSpd()), .0f, 32.0f);
+        ImGui::SliderFloat(std::string("gravity").c_str(), &(p.getRefGravityConst()), .0f, 32.0f);
+        ImGui::SliderFloat(std::string("max_fall_spd").c_str(), &(p.getRefMaxFallSpd()), -32.0f, .0f);
+        ImGui::SliderFloat(std::string("min_jump_spd").c_str(), &(p.getRefMinJumpSpd()), .0f, 32.0f);
         ImGui::SliderFloat(std::string("camera_x").c_str(), &dvar.camera_x, 0.0f, 20.0f);
         ImGui::SliderFloat(std::string("zoom").c_str(), &dvar.zoom_level, 1.0f, 50.0f);
         ImGui::End();
@@ -153,28 +150,28 @@ int DrawFrame(GLFWwindow* window, ImVec4& clear_color, PlayerSkills& pskills, Di
 
     // Camera follows player
     if (dvar.scroll_right) {
-        if (p->getPosX() / TILE_SIZE - dvar.camera_x > dvar.zoom_level * .35f / aspect_ratio) {
-            dvar.camera_x = elements->getP1()->getPosX() / TILE_SIZE - dvar.zoom_level * .35f / aspect_ratio;
+        if (p.getPosX() / TILE_SIZE - dvar.camera_x > dvar.zoom_level * .35f / aspect_ratio) {
+            dvar.camera_x = elements.getP1()->getPosX() / TILE_SIZE - dvar.zoom_level * .35f / aspect_ratio;
         }
-        else if (elements->getP1()->getPosX() / TILE_SIZE - dvar.camera_x < -dvar.zoom_level * .65f / aspect_ratio) {
-            dvar.camera_x = elements->getP1()->getPosX() / TILE_SIZE + dvar.zoom_level * .35f / aspect_ratio;
+        else if (elements.getP1()->getPosX() / TILE_SIZE - dvar.camera_x < -dvar.zoom_level * .65f / aspect_ratio) {
+            dvar.camera_x = elements.getP1()->getPosX() / TILE_SIZE + dvar.zoom_level * .35f / aspect_ratio;
             dvar.scroll_right = false;
         }
     }
     else {
-        if (p->getPosX() / TILE_SIZE - dvar.camera_x > dvar.zoom_level * .65f / aspect_ratio) {
-            dvar.camera_x = p->getPosX() / TILE_SIZE - dvar.zoom_level * .35f / aspect_ratio;
+        if (p.getPosX() / TILE_SIZE - dvar.camera_x > dvar.zoom_level * .65f / aspect_ratio) {
+            dvar.camera_x = p.getPosX() / TILE_SIZE - dvar.zoom_level * .35f / aspect_ratio;
             dvar.scroll_right = true;
         }
-        else if (p->getPosX() / TILE_SIZE - dvar.camera_x < -dvar.zoom_level * .35f / aspect_ratio) {
-            dvar.camera_x = p->getPosX() / TILE_SIZE + dvar.zoom_level * .35f / aspect_ratio;
+        else if (p.getPosX() / TILE_SIZE - dvar.camera_x < -dvar.zoom_level * .35f / aspect_ratio) {
+            dvar.camera_x = p.getPosX() / TILE_SIZE + dvar.zoom_level * .35f / aspect_ratio;
         }
     }
-    if (p->getPosY() / TILE_SIZE > dvar.zoom_level * .95f) {
-        dvar.zoom_level = p->getPosY() / TILE_SIZE / .95f;
+    if (p.getPosY() / TILE_SIZE > dvar.zoom_level * .95f) {
+        dvar.zoom_level = p.getPosY() / TILE_SIZE / .95f;
     }
-    else if (p->getPosY() / TILE_SIZE < -dvar.zoom_level * .95f) {
-        dvar.zoom_level = -(p->getPosY()) / TILE_SIZE / .95f;
+    else if (p.getPosY() / TILE_SIZE < -dvar.zoom_level * .95f) {
+        dvar.zoom_level = -(p.getPosY()) / TILE_SIZE / .95f;
     }
 
     // Draw map tiles
@@ -183,19 +180,19 @@ int DrawFrame(GLFWwindow* window, ImVec4& clear_color, PlayerSkills& pskills, Di
     mapTile.setU("camera_x", dvar.camera_x);
     mapTile.setU("aspect_ratio", aspect_ratio);
     MapDisplay* md = display.getMapDisplay();
-    for (int y = 0; y < elements->getMap()->getMapHeight(); y++) {
-        for (int x = 0; x < m->getMapHeight(); x++) {
-            if (m->isObstacle(x, y)) {
+    for (int y = 0; y < elements.getMap()->getMapHeight(); y++) {
+        for (int x = 0; x < m.getMapHeight(); x++) {
+            if (m.isObstacle(x, y)) {
                 mapTile.setU("pos", x, y);
                 mapTile.setU("color", 0.8f, 0.8f, 0.8f);
                 md->bindDrawTile();
             }
-            if (m->getTile(x, y) == TileType::slope45d) {
+            if (m.getTile(x, y) == TileType::slope45d) {
                 mapTile.setU("pos", x, y);
                 mapTile.setU("color", 0.8f, 0.1f, 0.8f);
                 md->bindDrawDTile();
             }
-            if (m->getTile(x, y) == TileType::slope45b) {
+            if (m.getTile(x, y) == TileType::slope45b) {
                 mapTile.setU("pos", x, y);
                 mapTile.setU("color", 0.1f, 0.8f, 0.8f);
                 md->bindDrawBTile();
@@ -206,7 +203,7 @@ int DrawFrame(GLFWwindow* window, ImVec4& clear_color, PlayerSkills& pskills, Di
     // Draw player
     playerIcon.use();
     playerIcon.setU("aspect_ratio", aspect_ratio);
-    playerIcon.setU("pos", p->getPosX() / TILE_SIZE, p->getPosY() / TILE_SIZE);
+    playerIcon.setU("pos", p.getPosX() / TILE_SIZE, p.getPosY() / TILE_SIZE);
     playerIcon.setU("zoom", 1 / dvar.zoom_level);
     playerIcon.setU("camera_x", dvar.camera_x);
     //playerIcon.setU("facing_left", elements.getP1().getFacingLeft());
@@ -214,8 +211,8 @@ int DrawFrame(GLFWwindow* window, ImVec4& clear_color, PlayerSkills& pskills, Di
     pd->bindDraw();
 
     // Draw ennemies A
-    for (int i = 0; i < elements->getEnnemiACount(); i++) {
-        goomba = elements->getEnnemiA(i);
+    for (int i = 0; i < elements.getEnnemiACount(); i++) {
+        goomba = elements.getEnnemiA(i);
         playerIcon.use();
         playerIcon.setU("aspect_ratio", aspect_ratio);
         playerIcon.setU("pos", goomba->getPosX() / TILE_SIZE, goomba->getPosY() / TILE_SIZE);
@@ -228,8 +225,15 @@ int DrawFrame(GLFWwindow* window, ImVec4& clear_color, PlayerSkills& pskills, Di
     // Draw ImGui elements
     ImGui::Render();
     ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-    glfwSwapBuffers(window);
+    glfwSwapBuffers(&window);
     return 1;
+}
+
+void Cleanup(GLFWwindow& window, LogicElements& elements, std::thread& t1) {
+    // Close application
+    elements.setShouldClose();
+    t1.join();
+    Cleanup(&window);
 }
 
 int main(int argc, char** argv) {
@@ -292,15 +296,12 @@ int main(int argc, char** argv) {
     std::unique_ptr<std::chrono::steady_clock> clock = std::make_unique<std::chrono::steady_clock>();
 
     // Start game logic thread
-    std::thread t1(logic, elements.get(), inputs.get(), clock.get());
-
+    std::thread t1(logic, *elements.get(), *inputs.get(), *clock.get());
+    
     // Main display loop
-    while (DrawFrame(window, clear_color, pskills, dvar, display, inputs.get(), elements.get(), m, p, playerIcon, mapTile)) {};
+    while (DrawFrame(*window, clear_color, pskills, dvar, display, *inputs.get(), *elements.get(), *m, *p, playerIcon, mapTile)) {};
      
-    // Close application
-    elements->setShouldClose();
-    t1.join();
-    Cleanup(window);
+    Cleanup(*window, *elements, t1);
 
     return 0;
 }
