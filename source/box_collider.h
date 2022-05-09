844bc4cfc656290b1679fc11d833ed0355b86d73
Hassim Mazahere Rakotomalala <hassimmazahere@gmail.com>
2022-05-09 13:54:21 +0200

    feat (box collider): add comments

--- source/box_collider.h
+++ source/box_collider.h
@@ -11,14 +11,48 @@ protected:
 public:
     BoxCollider();
     ~BoxCollider();
+
+    /// <summary>
+    /// Compares box's position to another box to see if they overlap/touch
+    /// </summary>
+    /// <param name="other">The other box object instance</param>
+    /// <returns>true if collision happens, false otherwise</returns>
     bool overlaps(BoxCollider const other);
+
+    /// <summary>
+    /// Returns the X position member variable
+    /// </summary>
+    /// <returns>int value of pos X</returns>
     int getPosX() const;
+
+    /// <summary>
+    /// Returns the Y position member variable
+    /// </summary>
+    /// <returns>int value of pos Y</returns>
     int getPosY() const;
-    int getHalfWidth() const; 
+
+    /// <summary>
+    /// Returns the half width member parameter
+    /// </summary>
+    /// <returns>int value of half width</returns>
+    int getHalfWidth() const;
+
+    /// <summary>
+    /// Returns the half height member parameter
+    /// </summary>
+    /// <returns>int value of half height</returns>
     int getHalfHeight() const;
 
+    /// <summary>
+    /// Set the absolute position of a box in the grid
+    /// </summary>
+    /// <param name="posx">X position of the grid to be set to</param>
+    /// <param name="posy">Y position of the grid to be set to</param>
     void setPos(int const posx, int const posy);
 
+    /// <summary>
+    /// WOYA REMOVES THIS
+    /// </summary>
     class MovingObject;
 };
 
